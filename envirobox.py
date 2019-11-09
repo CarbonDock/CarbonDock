44524ab92569f64b85f8b907e5633c41b8e6dd3d
iTecAI <matteovh@gmail.com>
2019-11-09 15:02:21 -0500

    change server

--- envirobox.py
+++ envirobox.py
@@ -3,4 +3,4 @@ from math import sin
 
 def get_values():
     #normally get sensor data here
-    return dict(co=3*sin(time()*0.1)+3,co2=3*sin((time()-5)*0.1)+3)
+    return dict(co=3*sin(time()*0.1)+3)
--- node/__init__.py
+++ node/__init__.py
@@ -4,9 +4,15 @@ sys.path.insert(1,os.path.join('..'))
 import carbonp2p
 from envirobox import get_values
 from socket import *
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-class CarbonFuncs:
-    pass
+class Handler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        pass
+
 
 if __name__ == '__main__':
     Node = carbonp2p.Node(6767,6768,'CarbonDock-'+gethostbyname(gethostname()),protocol='CarbonDock')
+    
+        
+
