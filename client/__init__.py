dbcdc6b224b2854a54b52d8a9f4b49d953481956
iTecAI <matteovh@gmail.com>
2019-11-09 16:47:43 -0500

    client edits

--- client/__init__.py
+++ client/__init__.py
@@ -1,14 +1,28 @@
 from __future__ import print_function	# For Py2/3 compatibility
 import eel
+from time import time
+from socket import *
 
 # Set web files folder
 eel.init('web')
 
-@eel.expose                         # Expose this function to Javascript
-def say_hello_py(x):
-    print('Hello from %s' % x)
+def discover(port,timeout=5):
+    s = socket(AF_INET, SOCK_DGRAM) #create UDP socket
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    s.bind(('', port))
+    curtime = time()
+    ret = {}
+    while time() < curtime+timeout:
+        data, addr = s.recvfrom(1024) #wait for a packet
+        data = str(data)[2:].strip("'")
+        if data.startswith('CarbonDock'):
+            data = data.split('|')
+            ret[data[1]] = data[2]
+    
+    return ret[1],ret[2]
 
-say_hello_py('Python World!')   # Call a Javascript function
-eel.say_hello_js('CarbonDock')
+@eel.expose
+def ips():
 
-eel.start('hello.html', size=(300, 200))    # Start
+
+eel.start('index.html', size=(300, 200))    # Start
