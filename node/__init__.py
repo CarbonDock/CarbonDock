797c6e74286366c0a3e4c6f31867dbdaa19dadd8
iTecAI <matteovh@gmail.com>
2019-11-09 15:34:30 -0500

    finish server 2.0

--- node/__init__.py
+++ node/__init__.py
@@ -1,7 +1,7 @@
 import sys,os
 sys.path.insert(1,os.path.join('..'))
 print([os.path.abspath(i) for i in sys.path])
-import carbonp2p
+from p2p import Node
 from envirobox import get_values
 from socket import *
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -40,7 +40,8 @@ class Handler(SimpleHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    Node = carbonp2p.Node(6767,6768,'CarbonDock-'+gethostbyname(gethostname()),protocol='CarbonDock')
-    
+    node = Node(6767,6768,'CarbonDock-'+gethostbyname(gethostname()),protocol='CarbonDock')
+    server = HTTPServer([gethostbyname(gethostname()),8000],Handler)
+    server.serve_forever()
         
 
