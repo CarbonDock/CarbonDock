f4c14e5c5b4aef770c0efa835e14363326bc115c
iTecAI <matteovh@gmail.com>
2019-11-09 15:25:04 -0500

    finish server

--- envirobox.py
+++ envirobox.py
@@ -3,4 +3,4 @@ from math import sin
 
 def get_values():
     #normally get sensor data here
-    return dict(co=3*sin(time()*0.1)+3)
+    return dict(co=200*sin(time()*0.05)+200)
--- node/__init__.py
+++ node/__init__.py
@@ -5,10 +5,91 @@ import carbonp2p
 from envirobox import get_values
 from socket import *
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from json import dump
 
 class Handler(SimpleHTTPRequestHandler):
+    def send_head(self,path=None):
+        #modified from http.server
+        if not path:
+            path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            parts = urllib.parse.urlsplit(self.path)
+            if not parts.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(HTTPStatus.MOVED_PERMANENTLY)
+                new_parts = (parts[0], parts[1], parts[2] + '/',
+                             parts[3], parts[4])
+                new_url = urllib.parse.urlunsplit(new_parts)
+                self.send_header("Location", new_url)
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            self.send_error(HTTPStatus.NOT_FOUND, "File not found")
+            return None
+
+        try:
+            fs = os.fstat(f.fileno())
+            # Use browser cache if possible
+            if ("If-Modified-Since" in self.headers
+                    and "If-None-Match" not in self.headers):
+                # compare If-Modified-Since and time of last file modification
+                try:
+                    ims = email.utils.parsedate_to_datetime(
+                        self.headers["If-Modified-Since"])
+                except (TypeError, IndexError, OverflowError, ValueError):
+                    # ignore ill-formed values
+                    pass
+                else:
+                    if ims.tzinfo is None:
+                        # obsolete format with no timezone, cf.
+                        # https://tools.ietf.org/html/rfc7231#section-7.1.1.1
+                        ims = ims.replace(tzinfo=datetime.timezone.utc)
+                    if ims.tzinfo is datetime.timezone.utc:
+                        # compare to UTC datetime of last modification
+                        last_modif = datetime.datetime.fromtimestamp(
+                            fs.st_mtime, datetime.timezone.utc)
+                        # remove microseconds, like in If-Modified-Since
+                        last_modif = last_modif.replace(microsecond=0)
+
+                        if last_modif <= ims:
+                            self.send_response(HTTPStatus.NOT_MODIFIED)
+                            self.end_headers()
+                            f.close()
+                            return None
+
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", ctype)
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified",
+                self.date_time_string(fs.st_mtime))
+            self.end_headers()
+            return f
+        except:
+            f.close()
+            raise
     def do_GET(self):
-        pass
+        with open('resp.json','w') as f:
+            x = int(get_values['co'])
+            dump(dict(
+                danger = 1.306837 + (0.02774721 - 1.306837)/(1 + (x/128.288)**2.844361)
+            ),f)
+        f = self.send_head(path='resp.json')
+        if f:
+            try:
+                self.copyfile(f, self.wfile)
+            finally:
+                f.close()
 
 
 if __name__ == '__main__':
